e6157b0c6ed9bc883bb7b4faba148767dd58e29c
Mark Morga <mmorga@rackspace.com>
2016-11-02 17:44:55 -0500

    removed commented out code

--- lib/archimate/diff/conflicts/path_conflict.rb
+++ lib/archimate/diff/conflicts/path_conflict.rb
@@ -38,34 +38,6 @@ module Archimate
             end
           end
         end
-
-        # def conflicts
-        #   @base_local_diffs.each_with_object([]) do |ldiff, cfx|
-        #     conflicting_remote_diffs =
-        #       @base_remote_diffs.select { |rdiff| ldiff.path == rdiff.path && ldiff != rdiff }.select do |rdiff|
-        #         if !(ldiff.array? && rdiff.array?)
-        #           true
-        #         else
-        #           case [ldiff, rdiff].map { |d| d.class.name.split('::').last }.sort
-        #           when %w(Change Change)
-        #             # TODO: if froms same and tos diff then conflict if froms diff then 2 sep changes else 1 change
-        #             ldiff.from == rdiff.from && ldiff.to != rdiff.to
-        #           when %w(Change Delete)
-        #             # TODO: if c.from d.from same then conflict else 1 c and 1 d
-        #             ldiff.from == rdiff.from
-        #           else
-        #             false
-        #           end
-        #         end
-        #       end.uniq
-
-        #     cfx << Conflict.new(
-        #       ldiff,
-        #       conflicting_remote_diffs,
-        #       "Conflicting changes"
-        #     ) unless conflicting_remote_diffs.empty?
-        #   end
-        # end
       end
     end
   end
