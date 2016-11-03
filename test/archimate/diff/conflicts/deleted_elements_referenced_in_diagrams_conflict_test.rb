09ba6dd2a9fef3a46d0e5738e764ab0a2881e655
Mark Morga <mmorga@rackspace.com>
2016-11-03 13:19:52 -0500

    Added archisurance example. Expanded conflict test.

--- test/archimate/diff/conflicts/deleted_elements_referenced_in_diagrams_conflict_test.rb
+++ test/archimate/diff/conflicts/deleted_elements_referenced_in_diagrams_conflict_test.rb
@@ -6,13 +6,23 @@ module Archimate
     class Conflicts
       class DeletedElementsReferencedInDiagramsConflictTest < Minitest::Test
         def setup
-          @model = build_model
-          @diff = Archimate::Diff::Delete.new("Model<#{@model.id}>/name", @model, @model.name)
-          @subject = DeletedElementsReferencedInDiagramsConflict.new([@diff], [])
+          @model = build_model(with_elements: 3, with_diagrams: 1)
+          @element = @model.elements.values.first
+          @diagram = @model.diagrams.values.first
+          @diff_name = Archimate::Diff::Delete.new("Model<#{@model.id}>/name", @model, @model.name)
+          @diff_del_element = Archimate::Diff::Delete.new("Model<#{@model.id}>/elements/#{@element.id}", @model, @element)
+          @diff_in_diagram = Archimate::Diff::Insert.new("Model<#{@model.id}>/diagrams/#{@diagram.id}/name", @model, @diagram.name)
+          @subject = DeletedElementsReferencedInDiagramsConflict.new([@diff_name, @diff_del_element], [])
         end
 
         def test_filter1
-          refute @subject.filter1.call(@diff)
+          refute @subject.filter1.call(@diff_name)
+          assert @subject.filter1.call(@diff_del_element)
+        end
+
+        def test_filter2
+          refute @subject.filter2.call(@diff_name)
+          assert @subject.filter2.call(@diff_in_diagram)
         end
       end
     end
